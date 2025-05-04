22af276556e68c7d470ed663d2ab24e7307e8ee1
SharkoStepan <144940807+SharkoStepan@users.noreply.github.com>
2025-05-04 19:01:37 +0300

    Update DelEventKeynodes.hpp

--- problem-solver/cxx/delEvent-module/keynodes/DelEventKeynodes.hpp
+++ problem-solver/cxx/delEvent-module/keynodes/DelEventKeynodes.hpp
@@ -4,9 +4,11 @@
 
 class DelEventKeynodes : public ScKeynodes {
 public:
-
-  static inline ScKeynode const NREL_EVENT_NAME{"nrel_event_name", ScType::ConstNodeNonRole};
-
+  // Узел действия агента удаления
   static inline ScKeynode const action_del_event{"action_del_event", ScType::NodeConstClass};
 
+  // Данные для ввода пользователем
+  static inline ScKeynode const NREL_EVENT_NAME{"nrel_event_name", ScType::ConstNodeNonRole};
+  static inline ScKeynode const NREL_USER_EVENT{"nrel_user_event", ScType::NodeConstNoRole};
+  static inline ScKeynode const REGISTERED_USER{"registered_jurisprudence_user", ScType::NodeConstClass};
 };
