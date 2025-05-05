32211a67a544f821d6f3a231c7cb724f884f89b5
SharkoStepan <144940807+SharkoStepan@users.noreply.github.com>
2025-05-06 02:43:08 +0300

    Update DelEventAgent.hpp

--- problem-solver/cxx/delEvent-module/agents/DelEventAgent.hpp
+++ problem-solver/cxx/delEvent-module/agents/DelEventAgent.hpp
@@ -4,6 +4,7 @@
 
 class DelEventAgent : public ScActionInitiatedAgent {
 public:
-  ScAddr GetActionClass() const override;
-  ScResult DoProgram(ScAction & action) override;
+    ScAddr GetActionClass() const override;
+    ScResult DoProgram(ScAction & action) override;
+    void remove_event_link(ScAddr const & userAddr, std::string const & eventName);
 };
